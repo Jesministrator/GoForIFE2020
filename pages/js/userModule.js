0973da760203ed9339dbdecfaf28c5b259049200
jesministrator <dingjwilliams@163.com>
2020-05-22 14:43:10 +0800

    完成用户名的正确和错误提醒框

--- pages/js/userModule.js
+++ pages/js/userModule.js
@@ -12,19 +12,20 @@ $(document).ready(function(){
             if(flag){
                 var name = $(this).val();
                 if(name.length==0){
-                    hideTooltips("name_tooltip");
+                    hideTooltips("name_tooltip");   
                 }else if(name.length<2){
                     hideTooltips("name_tooltip");
-                    showTooltips("name_tooltip","用户名过短，最少2位");
+                    showTooltips("name_tooltip","用户名过短，最少2位","");
                 }else{
                     hideTooltips("name_tooltip");
                     if(usernameReg(name)){
                         console.log("value:"+$(this).val()+"   length:"+$(this).val().length);
                         hideTooltips("name_tooltip");
+                        showTooltips("name_tooltip","用户名合法","right");
                     }else{
                         hideTooltips("name_tooltip");
                         console.log(name);
-                        showTooltips("name_tooltip","英文或数字构成，不能以数字开头");
+                        showTooltips("name_tooltip","英文或数字构成，不能以数字开头","");
                         console.log("用户名由中文,英文或数字构成，不能以数字开头");
                     }
                 }
@@ -35,10 +36,18 @@ $(document).ready(function(){
         
     })
 })
-function showTooltips(msgid,msg){
+function showTooltips(msgid,msg,suf){
+    var base_t = "tooltips";
+    var base_m = "msg";
+    var base_tb = "tooltips_box";
+    if(suf.length!=0){
+        base_t = base_t+"_"+suf;
+        base_m = base_m+"_"+suf;
+        base_tb = base_tb+"_"+suf;
+    }
 	if (msgid==''){ return; }
     if (msg==''){ msg='Error!'; }
-	$('#'+msgid).prepend("<div class='for_fix_ie6_bug' style='position:relative;'><div class='tooltips_main'><div class='tooltips_box'><div class='tooltips'><div class='msg'>"+msg+"</div></div><div class='ov'></div></div></div></div>");
+	$('#'+msgid).prepend("<div style='position:relative;'><div class='tooltips_main'><div class='"+base_tb+"'><div class='"+base_t+"'><div class='"+base_m+"'>"+msg+"</div></div><div class='ov'></div></div></div></div>");
 	$('#'+msgid+' .tooltips_main').fadeIn("slow").animate({ marginTop: "7px"}, {queue:true, duration:400});
 }
 
